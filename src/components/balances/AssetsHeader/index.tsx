847728f7266f374769a53e098b62e47952f332e4
Siddarth Vijay <vsiddarthvijay@gmail.com>
2023-01-09 20:36:02 +0530

    removed currency switcher

--- src/components/balances/AssetsHeader/index.tsx
+++ src/components/balances/AssetsHeader/index.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from 'react'
 
 import NavTabs from '@/components/common/NavTabs'
 import PageHeader from '@/components/common/PageHeader'
-import CurrencySelect from '@/components/balances/CurrencySelect'
+// import CurrencySelect from '@/components/balances/CurrencySelect'
 import { balancesNavItems } from '@/components/sidebar/SidebarNavigation/config'
 
 const AssetsHeader = ({ currencySelect = false }: { currencySelect?: boolean }): ReactElement => {
@@ -13,7 +13,7 @@ const AssetsHeader = ({ currencySelect = false }: { currencySelect?: boolean }):
       action={
         <Box display="flex" justifyContent="space-between" alignItems="center">
           <NavTabs tabs={balancesNavItems} />
-          {currencySelect && <CurrencySelect />}
+          {/* {currencySelect && <CurrencySelect />} */}
         </Box>
       }
     />
