51169b132985950c64715116700ae170dadbdd50
Siddarth Vijay <vsiddarthvijay@gmail.com>
2023-01-11 12:03:07 +0530

    fix typograph overrides; amplify build fixed

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -33,6 +33,25 @@ declare module '@mui/material/styles' {
   interface SimplePaletteColorOptions {
     background?: string
   }
+
+  // Custom typography variants
+  interface TypographyVariants {
+    body3: React.CSSProperties
+    body4: React.CSSProperties
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    body3?: React.CSSProperties
+    body4?: React.CSSProperties
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    body3: true
+    body4: true
+  }
 }
 
 declare module '@mui/material/SvgIcon' {
